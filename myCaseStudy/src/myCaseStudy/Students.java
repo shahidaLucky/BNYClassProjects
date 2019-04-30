50fa7195838746d6d8db45e1ba3edd39b2e7a227
shahida.lucky <shahida.lucky@EC2AMAZ-4NET5CM.corp.amazonworkspaces.com>
2019-04-29 23:25:33 -0400

    initial commit

---
+++ myCaseStudy/src/myCaseStudy/Students.java
@@ -0,0 +1,14 @@
+package myCaseStudy;
+
+public class Students {
+	int studentId;
+	String firstName;
+	String lastName;
+	String address;
+	
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
