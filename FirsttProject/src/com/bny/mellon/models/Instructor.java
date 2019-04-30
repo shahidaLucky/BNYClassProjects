d980d4ee01ef43cbfee9f7a4738babf1828fd13f
shahida.lucky <shahida.lucky@EC2AMAZ-4NET5CM.corp.amazonworkspaces.com>
2019-04-29 21:15:07 -0400

    first commit

---
+++ FirsttProject/src/com/bny/mellon/models/Instructor.java
@@ -0,0 +1,59 @@
+package com.bny.mellon.models;
+
+public class Instructor {
+
+	private String fullName;
+	private String courseName;
+	private String employeer;
+	private Double salary;
+	/**
+	 * @return the fullName
+	 */
+	public String getFullName() {
+		return fullName;
+	}
+	/**
+	 * @param fullName the fullName to set
+	 */
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	/**
+	 * @return the courseName
+	 */
+	public String getCourseName() {
+		return courseName;
+	}
+	/**
+	 * @param courseName the courseName to set
+	 */
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}
+	/**
+	 * @return the employeer
+	 */
+	public String getEmployeer() {
+		return employeer;
+	}
+	/**
+	 * @param employeer the employeer to set
+	 */
+	public void setEmployeer(String employeer) {
+		this.employeer = employeer;
+	}
+	/**
+	 * @return the salary
+	 */
+	public Double getSalary() {
+		return salary;
+	}
+	/**
+	 * @param salary the salary to set
+	 */
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+	
+	
+}
---
+++ FirsttProject/src/com/bny/mellon/models/Main.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.bny.mellon.models;
+
+/**
+ * @author shahida.lucky
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println();
+		System.out.println("Not a Hello World");
+	}
+
+}
